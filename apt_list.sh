d6c9bd7d440ee6c63023e51083028834e38b3944
volmer <volmer@tlen.pl>
2018-02-02 13:08:09 +0100

    ok

--- apt_list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get -s upgrade > /tmp/apt_list_out
-lista=`cat /tmp/apt_list_out`
-aktualizacje=`cat /tmp/apt_list_out | grep upgraded`
-echo $aktualizacje
-#echo "log ze sprawdzenia"
-#echo $lista
-
-
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dialog --title "Okno msgbox" --backtitle "Kurs programowania w shellu" --msgbox "Tutaj można wstawić jakikolwiek tekst, na
-przykład informacje o tym co skrypt robi,
-pomoc, itp." 7 45
